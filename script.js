19cd8764fb1d02cd8b181404e413ba862912ada7
Sri Lakshmi Sowmya Bangaru <144713399+SRKRECgithub@users.noreply.github.com>
2025-03-21 16:56:18 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<script>
-  function redirectToHome() {
-    window.location.href = 'homel.html'; // Change 'index.html' to your home page URL if different
-  }
-</script>
+function redirectToHome() {
+    window.location.href = 'homel.html';
+}
